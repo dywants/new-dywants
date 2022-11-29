427c798a6b787c6ddce273fdead290db6b2c1dee
Cyrille Thibaut BAYONG <dywantscm@gmail.com>
2022-11-29 05:26:33 +0100

    clear web.php file

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ use App\Http\Controllers\ZimbraIndexController;
 |
 */
 
-/* Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-}); */
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
