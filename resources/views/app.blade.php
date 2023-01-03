610d605e38150ff2953d0e28b036eab3ad5383ee
Cyrille Thibaut BAYONG <dywantscm@gmail.com>
2023-01-03 21:50:48 +0100

    change script track matomo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,14 +24,14 @@
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
-     <!-- Matomo -->
+ <!-- Matomo -->
 <script>
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//matomo.dywantshost.com/";
+    var u="//matomo.dywants.com/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
